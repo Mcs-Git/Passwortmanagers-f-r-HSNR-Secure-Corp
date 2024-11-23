0f5d0ab1bb28687050b2a95e0dbf8f9d68882ea4
Mcs-Git <mcfrankel80@gmail.com>
2024-11-23 01:50:15 +0100

    updated function_speichern.py

--- function_speichern.py
+++ function_speichern.py
@@ -3,22 +3,50 @@ import os
 import bcrypt as end_encryption
 from cryptography.hazmat.primitives import hashes, hmac
 
+
+
+#Verchlüsselt das Passwort und Gib einen Hashwert in hexadezimal raus.
 def password_encryption(entered_password):
     if not os.path.exists("users_db.txt"):
         key = os.urandom(256)
         kr.set_password("Secret","Key",key)
-        print(type(key))
     generated_key = kr.get_password("Secret", "Key").encode("ASCII")
-    print("Password", type(generated_key))
     h = hmac.HMAC(generated_key, hashes.SHA256())
     entry_toByte = entered_password.encode("ASCII")
     h.update(entry_toByte)
     signature = h.finalize()
 
-    password_hashed = end_encryption.hashpw(signature, end_encryption.gensalt())
-    print("Signature3: ", password_hashed.hex())
-    return password_hashed
+    #password_hashed = end_encryption.hashpw(signature, end_encryption.gensalt(10))
+    return signature.hex()
 
+#Gib eine Liste von allen Benutzern oder Passwörtern abhängig von dem index_to_retrieve aus:
+#  0 = Benutzer
+#  1 = Passwörter
+
+def retrieve_data(index_to_retrieve):
+    file_table = []
+    hash_users_table = []
+    with open("users_db.txt", "r") as f:
+        for line in f.readlines():
+            file_table.append(line.strip("[\n]").split(","))
+
+    for i in range(0, len(file_table)):
+        hash_password = str(file_table[i][index_to_retrieve]).split("'")[1]
+        hash_users_table.append(hash_password)
+    return hash_users_table
+
+
+def user_verification(username_entered):
+    if username_entered in retrieve_data(0):
+        print("User Found")
+    else:
+        print("User Not Found")
+def password_verification(password_entered):
+    hashed_password = password_encryption(password_entered)
+    if hashed_password in retrieve_data(1):
+        return True
+    else:
+        return False
 
 #Erstellung eines/einer Benutzer:in
 class Benutzer:
@@ -27,24 +55,22 @@ class Benutzer:
         self.password = password
 
     def __str__(self):
-        return f"{[self.name, password_encryption(self.password)]}"
+        return f"{[self.name, password_encryption(self.password+self.name)]}"
+
+if __name__ == '__main__':
+    user_name = input("Benutzername: ")
+    user_password = input("Benutzerpasswort: ")
+    user_obj = Benutzer(user_name, user_password)
+    print(user_obj)
 
 
+    #Überprüft, ob die Datei existiert
+    if not os.path.exists("users_db.txt"):
+        with open("users_db.txt", "w"):
+            pass
 
-#print(password_encryption('Mc123'))
-#var = "Hey".encode("ASCII")
-#print(var)
-user_name = input("Benutzername: ")
-user_password = input("Benutzerpasswort: ")
-user_obj = Benutzer(user_name, user_password)
-print(user_obj)
+    with open("users_db.txt", "a") as f:
+        f.write(f"{user_obj}\n")
+    result = password_verification("Thomasuser")
+    print(result)
 
-#Überprüft, ob die Datei existiert
-if not os.path.exists("users_db.txt"):
-    with open("users_db.txt", "w"):
-        pass
-
-with open("users_db.txt", "a") as f:
-    f.write(f"{user_obj}\n")
-
-#b'\x90\x03\x01z\x984A\x7f\xb5\xbf\xcb}\xe7l\xd9\x9e0\xf79jNi`\x89\xcc\xef\xbc\x1e)=\xb61\x97A\x01\x1a\xdb\x10\xf5`Mu\xea\xd5\xceE\x91\xd6`\xed-\x83;bu\xb7\xb3\x04\x90|\x96\x9d\xaf$\x88\xea\xd6gHr\x92\x12\x88\x8c\x80i]6\xf9{\xe0mJ\xfdEZ\x1b\xa1\x11\x92\xd90D\xb3nR\xd4$x\x1d\xd6\xb8w`3"\'Y\x04\\\x1b\x94\xc9Nb\x83%o\x06W\x117N\x82\x8d\xb6\x85-\xfd\xb8\xca\xe5\x8e\xd6\x12\xb6\x90\xb1\xd
